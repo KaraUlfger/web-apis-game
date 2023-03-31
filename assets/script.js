e75454d7b884b6c44baf24caae508529033ad621
Kara <karaulf@uw.edu>
2023-03-31 15:06:44 -0700

    adding psuedo code

---
+++ assets/script.js
@@ -0,0 +1,5 @@
+// Click start button and timer starts for quiz
+// After timer starts, various questions from ARRAY to answer from multiple choice ARRAY
+// if correct answer=move on to next question. Else if incorrect answers subtract from timer and move onto next question
+// if all questions answered = game ends, add up score, else if time runs out = game ends, add up score
+// game over, adds up score, and user inputs initals to be saved in "Score Section"
